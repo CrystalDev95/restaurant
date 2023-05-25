7bfddfd4f07cef941f5ba1b021ef9a18367d07cd
unknown <crystalyeo123@hotmail.com>
2023-05-26 11:44:07 +1200

    fixed wiggle.json

--- src/components/utils/WiggleLine.jsx
+++ src/components/utils/WiggleLine.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Lottie from "lottie-react";
-import Wiggle from '../../assets/animation/Wiggle.json'
+import Wiggle from '../../assets/animation/wiggle.json'
 
 const WiggleLine = () => {
     return (
